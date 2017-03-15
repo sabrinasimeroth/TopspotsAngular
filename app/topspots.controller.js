5041170f4885df7dc63329d01030c6e9f4b1c495
Sabrina  Simeroth <SabrinaSimeroth@Macintosh-3.local>
2017-03-14 20:58:32 -0700

    added css and styling to buttons

---
+++ app/topspots.controller.js
@@ -0,0 +1,15 @@
+angular
+  .module('tsApp')
+  .controller('HomeController', HomeController);
+
+  function HomeController($http){
+    var vm = this;
+
+    $http.get('https://raw.githubusercontent.com/OriginCodeAcademy/Cohort10/master/Projects/04-SanDiegoTopSpots/topspots.json')
+      .then(function(response){
+        vm.info = response.data;
+
+    });
+
+
+  }
